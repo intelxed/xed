c6e4a63e0c49f251a6ff4c665aa56b51a6e90a7f
Mark Charney <mark.charney@intel.com>
2017-03-27 08:55:37 -0400

    override-able chip mode operand settings
      * allow setting operands based chip & feature settings. The
      xed-chi-modes-override.c file can be replaced in extended builds.
    
    Change-Id: I2452bd56372fb7cd78c0c61c6f03381e7406d532
    (cherry picked from commit 872552242193cd0b50c8cc98f5b6f61e0c17cbbd)

---
+++ include/private/xed-chip-modes-override.h
@@ -0,0 +1,29 @@
+/*BEGIN_LEGAL 
+
+Copyright (c) 2017 Intel Corporation
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+  
+END_LEGAL */
+
+#if !defined(XED_CHIP_MODES_OVERRIDE)
+# define XED_CHIP_MODES_OVERRIDE
+
+# include "xed-chip-enum.h"
+# include "xed-decoded-inst.h"
+# include "xed-chip-features.h"
+
+void xed_chip_modes_override(xed_decoded_inst_t* xedd,
+                             xed_chip_enum_t chip,
+                             xed_chip_features_t* features);
+#endif
