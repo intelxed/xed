e7d734962c4809a11d0cd8012c8c4001871d5f47
Mark Charney <mark.charney@intel.com>
2017-06-11 13:30:05 -0400

    update legal header & date for py3 ported files
    Change-Id: I166833daaa56c33eca01bdf7b9aa6e74a490ba9a
    (cherry picked from commit 1212ba962dff6dfbfa0bd2469327ff447ce59058)

--- pysrc/classify_keys.py
+++ pysrc/classify_keys.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #BEGIN_LEGAL
 #
-#Copyright (c) 2016 Intel Corporation
+#Copyright (c) 2017 Intel Corporation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
--- pysrc/enum_txt_writer.py
+++ pysrc/enum_txt_writer.py
@@ -4,7 +4,7 @@
 # Enumeration support
 #BEGIN_LEGAL
 #
-#Copyright (c) 2016 Intel Corporation
+#Copyright (c) 2017 Intel Corporation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
