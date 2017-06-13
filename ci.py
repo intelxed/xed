908f1219e11c25542fb3f590e7ea1867d10e1404
Mark Charney <mark.charney@intel.com>
2017-06-13 12:02:17 -0400

    travis: split build subdirs
    Change-Id: Ieef2d110c7ebab0f50363d35ecdebeae244c8384

--- ci.py
+++ ci.py
@@ -6,15 +6,16 @@ def get_python_cmds():
     if platform.system() == 'Linux':
         for pyver in ['2.7','3.5.2']:
             pycmd = '/opt/python/{}/bin/python'.format(pyver)
-            lst.append(pycmd)
+            lst.append((pyver,pycmd))
         return lst
     
-    return ['python']
+    return [('dfltpython','python')]
         
     
-for pycmd in get_python_cmds():
+for pyver,pycmd in get_python_cmds():
     for size in ['ia32','x86-64']:
-        for link in ['','--shared']:
-            cmd = '{} mfile.py --build-dir=build host_cpu={} {} test'.format(pycmd,size,link)
+        for linkkind,link in [('dfltlink',''),('dynamic','--shared')]:
+            build_dir = '{}-{}-{}'.format(pyver, size, linkkind)
+            cmd = '{} mfile.py --build-dir=build-{} host_cpu={} {} test'.format(pycmd,build_dir,size,link)
             print(cmd)
             subprocess.check_call(cmd, shell=True)
