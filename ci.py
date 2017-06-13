c5bea9dfb32dd328a5e00852f54ed4d41016b9db
Mark Charney <mark.charney@intel.com>
2017-06-13 11:56:58 -0400

    travis: trying using  multiple python versions on linux
    Change-Id: I11f4fca10f5c7648cc2a605ab678789bce7200c5

--- ci.py
+++ ci.py
@@ -2,8 +2,19 @@ import os
 import platform
 import subprocess
 
-for size in ['ia32','x86-64']:
-    for link in ['','--shared']:
-        cmd = 'python mfile.py --build-dir=build host_cpu=%s %s test' % (size,link)
-        print(cmd)
-        subprocess.check_call(cmd, shell=True)
+def get_python_cmds():
+    if platform.system() == 'Linux':
+        for pyver in ['2.7','3.5.2']:
+            pycmd = '/opt/python/{}/bin/python'.format(pyver)
+            lst.append(pycmd)
+        return lst
+    
+    return ['python']
+        
+    
+for pycmd in get_python_cmds():
+    for size in ['ia32','x86-64']:
+        for link in ['','--shared']:
+            cmd = '{} mfile.py --build-dir=build host_cpu={} {} test'.format(pycmd,size,link)
+            print(cmd)
+            subprocess.check_call(cmd, shell=True)
