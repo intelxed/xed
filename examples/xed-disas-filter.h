91f7ab777b193d8066343524a855cf3735ec8a34
Mark Charney <mark.charney@intel.com>
2018-02-09 16:36:30 -0500

    update copyright headers for 2018, add missing headers
    Change-Id: Ica2ca1054393e28ed1759d9dcdb79c7f47127cbe
    (cherry picked from commit 29da1ff99e456d23afdac7440a8ff159919baae9)

--- examples/xed-disas-filter.h
+++ examples/xed-disas-filter.h
@@ -1,6 +1,6 @@
-/*BEGIN_LEGAL
+/*BEGIN_LEGAL 
 
-Copyright (c) 2017 Intel Corporation
+Copyright (c) 2018 Intel Corporation
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@ Copyright (c) 2017 Intel Corporation
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
   See the License for the specific language governing permissions and
   limitations under the License.
-
+  
 END_LEGAL */
 
 int disas_filter(xed_decoded_inst_t *inst, char *prefix, xed_disas_info_t *di);
