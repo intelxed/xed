16ff6c456cb11c4359761590916b7eead7927753
Mark Charney <mark.charney@intel.com>
2019-04-08 12:26:12 -0400

    cpuid_rdr.py: spacing fix. trivial
    Change-Id: I93f94dcd7b56dcfe247ebdbc0865c92a92a8abc2
    (cherry picked from commit a5c74b723fbcdc8c259b451fcab69c1dff83a359)

--- pysrc/cpuid_rdr.py
+++ pysrc/cpuid_rdr.py
@@ -27,7 +27,7 @@ def read_file(fn):
         isa_set = wrds[0].strip()
         cpuid_bits = wrds[1].upper().split()
         if isa_set in d:
-            msg = "Duplicate cpuid definition for isa set. isa-set={} old ={} new={}"
+            msg = "Duplicate cpuid definition for isa set. isa-set={} old={} new={}"
             genutil.die(msg.format(isa_set, d[isa_set], cpuid_bits))
         d[isa_set] = cpuid_bits
     return d
