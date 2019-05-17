bd555ed7cf973118bf67eed89d2d33b822e05594
Mark Charney <mark.charney@intel.com>
2019-05-17 17:02:37 -0400

    enc2: add API for jamming a legacy prefix byte into the output buffer
    Change-Id: Ic59cc6a1f83d97b8b59a041054488af57504efc6
    (cherry picked from commit d5cb03f0b9911462b42e9d76b760954c58ad17f0)

--- src/enc2/xed-encode-direct.c
+++ src/enc2/xed-encode-direct.c
@@ -1361,4 +1361,15 @@ xed_uint8_t emit_partial_opcode_and_rmreg_gpr64(xed_enc2_req_t* r,
 }
 
 
+void emit_seg_prefix(xed_enc2_req_t* r,
+                     xed_reg_enum_t reg) {
+    static const xed_uint8_t seg_prefixes[] = { 0x26, 0x2e, 0x36, 0x3e, 0x64, 0x65 };
+    if (reg >= XED_REG_ES && reg <= XED_REG_GS) {
+        xed_uint_t offset = reg - XED_REG_ES;
+        emit_u8(r,seg_prefixes[offset]);
+    }
+    else {
+        xed_enc2_error("Bad segment register prefix");
+    }
+}
 
