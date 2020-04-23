1f508b0eaac32df4cd3526eb6a07f141d64141b9
Mark Charney <mark.charney@intel.com>
2020-04-23 09:09:00 -0400

    ci-internal.py: cloning mbuild from gitlab.
    Change-Id: I6d962f8791c049d1ee132807cb40435d6220f274
    (cherry picked from commit 78609879cc7d50100542021a68dca0b2e9d41798)

---
+++ ci-internal.py
@@ -0,0 +1,46 @@
+#BEGIN_LEGAL
+#
+#Copyright (c) 2019 Intel Corporation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  
+#END_LEGAL
+import os
+import platform
+import subprocess
+
+def get_python_cmds():
+    if platform.system() == 'Windows':
+        return [ (x,'C:/python{}/python'.format(x)) for x in ['37'] ]
+    elif platform.system() in ['Darwin','Linux']:
+        # The file .travis.yml installs python3 on linux. Already present on mac
+        return [ ('3.x','python3')]
+    return [('dfltpython','python')]
+        
+    
+for pyver,pycmd in get_python_cmds():
+    mbuild_git='https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.devtools.intel.com/xed-group/mbuild.git'
+    cmd = 'git clone {} mbuild'.format(mbuild_git)
+    print(cmd)
+    subprocess.check_call(cmd, shell=True)
+    
+    cmd = '{} -m pip install --user ./mbuild'.format(pycmd)
+    print(cmd)
+    subprocess.check_call(cmd, shell=True)
+    
+    for size in ['ia32','x86-64']:
+        for linkkind,link in [('dfltlink',''),('dynamic','--shared')]:
+            build_dir = '{}-{}-{}'.format(pyver, size, linkkind)
+            cmd = '{} mfile.py --build-dir=build-{} host_cpu={} {} test'.format(pycmd,build_dir,size,link)
+            print(cmd)
+            subprocess.check_call(cmd, shell=True)
