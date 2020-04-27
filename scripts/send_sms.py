ed788d5d6162ca2f3feade8c6d2903a00f70e342
Mark Charney <mark.charney@intel.com>
2020-04-26 20:19:56 -0400

    simplify send_sms.py
    (cherry picked from commit c93e66c548bb188610a317eee86a596935f4e1af)

--- scripts/send_sms.py
+++ scripts/send_sms.py
@@ -25,97 +25,34 @@
 #END_LEGAL
 
 import argparse
-import itertools
 import zmail
 import os
 import sys
 
-
-class Recipient(object):
-    def __init__(self, phone, service='wintel'):
-        self.phone = phone
-        self.service = service
-
 def _getenv_phone():
     if 'SMS_PHONE' in os.environ:
-      return os.environ['SMS_PHONE']
+        return os.environ['SMS_PHONE']
     sys.stderr.write("No phone number in SMS_PHONE env var\n")
-    sys.exit(1)
+    return None
 
-
-phone_book = {
-   'mark'       : Recipient(_getenv_phone(),'att'),
-   'mjc'        : Recipient(_getenv_phone(),'att'),
-   'mjcharne'   : Recipient(_getenv_phone(),'att'),
-   }
-
-
-def _send_att(message, recipients, sender, userid):
-    """
-    Sends sms through AT&T.
-
-    Simply sends email to the 10 digit phone number followed by @txt.att.net
-    Example: xxxxxxxxxx@txt.att.net
-    """
-    recipients = ['%s@txt.att.net' % phone for phone in recipients]
-    sender = '%s@txt.att.net' % sender
+def _send_email_gateway(message, recipients):
+    sender = recipients[0]
     zmail.mail([message], sender, recipients)
 
-
-def send(message, recipients, sender=None, userid='automated-script'):
-    """
-    Sends SMS message to a list of recipients.
-
-    :param message: sms message
-    :param recipients: recipients' phone number list
-    :param sender: sender's phone number
-    :param userid: unused.
-    :return:
-    """
-
-    def _name_to_recipient(name):
-        return phone_book.get(name, Recipient(name))
-
-    def _recipient_service(recipient):
-        return recipient.service
-
-    recipients = list(map(_name_to_recipient, recipients))
-
-    # by default, the sender is the (first) recipient
-    if not sender:
-        sender = recipients[0].phone
-    else:
-        sender = _name_to_recipient(sender).phone
-
-    send_function_map = {
-                            'att'    : _send_att,
-                        }
-    recipients_by_service = itertools.groupby(recipients, _recipient_service)
-
-    for (service,recipients_in_group) in recipients_by_service:
-        phones = [recipient.phone for recipient in recipients_in_group]
-        send_func = send_function_map[service]
-        send_func(message, phones, sender, userid)
-
-
-def setup():
+def _setup():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--to', action='append', required=True, type=str,
-                        help='recipient phone number or name',
-                        metavar='phone_num|name')
-    parser.add_argument('-s', '--sender', type=str,
-                        help='sender phone number or name', metavar='phone_num|name',
-                        default=None)
-    parser.add_argument('-m', '--msg', required=True, type=str,
-                        help='sms message body')
+    parser.add_argument('message', help='message')
     args = parser.parse_args()
     return args
 
-
-
-def main(args):
-    send(args.msg, recipients=args.to, sender=args.sender)
+def _main(args):
+    recipient = _getenv_phone()
+    if recipient:
+        _send_email_gateway(args.message,[recipient])
+        return 0
+    return 1
 
 if __name__ == '__main__':
-    args = setup()
-    main(args)
+    args = _setup()
+    r = _main(args)
+    sys.exit(r)
