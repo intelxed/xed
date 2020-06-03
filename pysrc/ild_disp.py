3d417eb06743127a54522daa106315345aa82377
B. Wilson <esoochucho@wilsonb.com>
2020-06-03 18:35:03 +0900

    Remove sources of non-determinism in library builds
    Prior to this commit, the following object files built
    non-deterministically:
    
    * xed-ild-disp-l3.o,
    * xed-ild-eosz.o, and
    * xed-ild.o.
    
    This breaks the ability to build the library reproducibly. The primary
    source of non-determinism comes from Python dictionaries placing no
    guarantees on the order of items returned from `list(dict)`.
    
    A simple workaround is to use `sorted(dict.keys())` or
    `sorted(dict.items())` etc. as appropriate. However,
    `sorted(dict.values())` fails to work in the general case since
    dictionary values may not be orderable. For sorted `dict.items()` the
    following idiom suffices:
    
        [value from (key,value) in sorted(dict.items())]
    
    which this patch uses to replace instances of `list(dict.values())` in
    several places.
    
    Tracking down the particular offending object files was relatively
    straightforward:
    
    1) Build the library twice, using separate kit directories,
    2) Use `objcopy -D` to sanitize obvious non-determinism from the
       archives, and finally
    3) Use `cmp` to find the differing files.
    
    This turned up `libxed.a` as the offender. Given this is an object
    *archive*, we need to extract its object files and find the problematic
    ones:
    
        $ ar x --output out/A kit-A/lib/libked.a
        $ ar x --output out/B kit-B/lib/libked.a
    
    Running `cmp` over these object files turned up the mismatches mentioned
    above. The output of `cmp` also gives the offset of the first
    mismatching bytes, and consulting `readelf -S` shows these offsets to be in
    the `.text` sections, which tells us that the problem is in the code
    generation itself rather than simply some ELF metadata.
    
    A quick and dirty comparison of the disassembled code (via `objdump -d`)
    makes it obvious that the problem comes from differing *orders* of the
    code blocks for each symbol, i.e. the code generators are simply
    outputting functions in a different order between runs.

--- pysrc/ild_disp.py
+++ pysrc/ild_disp.py
@@ -350,7 +350,8 @@ def work(agi, united_lookup,  disp_nts, brdisp_nts, ild_gendir,
     disp_dict = _gen_l3_array_dict(agi, disp_nts, _disp_token)
 
     
-    nt_arr_list = list(brdisp_dict.values()) + list(disp_dict.values())
+    nt_arr_list = ([v for (k,v) in sorted(brdisp_dict.items())] +
+                   [v for (k,v) in sorted(disp_dict.items())])
     #create function that calls all initialization functions
     init_f = ild_nt.gen_init_function(nt_arr_list, 'xed_ild_disp_l3_init')
     
@@ -367,7 +368,7 @@ def work(agi, united_lookup,  disp_nts, brdisp_nts, ild_gendir,
     l2_functions = []
     eosz_op = ild_eosz.get_target_opname()
     easz_op = ild_easz.get_target_opname()
-    for nt_name,array in list(disp_dict.items()) + list(brdisp_dict.items()):
+    for nt_name,array in sorted(disp_dict.items()) + sorted(brdisp_dict.items()):
         #Some DISP NTs depend on EOSZ, others on EASZ, we need to know
         #that when we generate L2 functions
         if eosz_op in array.get_arg_names():
