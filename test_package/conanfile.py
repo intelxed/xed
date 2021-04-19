408dfb75be9791fa97455efe6201c1346382d15e
Alexey Klimkin <alexey.d.klimkin@intel.com>
2021-04-19 16:43:16 -0700

    Fix Python lint warning

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,6 +1,6 @@
-#BEGIN_LEGAL
+# BEGIN_LEGAL
 #
-#Copyright (c) 2021 Intel Corporation
+# Copyright (c) 2021 Intel Corporation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-#END_LEGAL
+# END_LEGAL
 
 from conans import CMake, ConanFile
 
