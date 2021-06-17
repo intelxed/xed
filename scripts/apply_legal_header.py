74d10ca2a5a4ea4debe4f834bcabd06c89129fec
Mark Charney <mark.charney@intel.com>
2021-06-17 09:14:52 -0400

    apply_legal_header.py: comply with PEP 8, space after hash char

--- scripts/apply_legal_header.py
+++ scripts/apply_legal_header.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python 
 # -*- python -*-
-#BEGIN_LEGAL
-#
-#Copyright (c) 2019 Intel Corporation
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-#  
-#END_LEGAL
+# BEGIN_LEGAL
+# 
+# Copyright (c) 2021 Intel Corporation
+# 
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#       http://www.apache.org/licenses/LICENSE-2.0
+# 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#   
+# END_LEGAL
 from __future__ import print_function
 import sys
 import os
@@ -54,7 +54,7 @@ def prepend_script_comment(header):
     "Apply script comment marker to each line"
     retval = []
     for line in header:
-        retval.append( "#" + line )
+        retval.append( "# " + line )
     return retval
 
 def apply_header_to_source_file(header, file):
