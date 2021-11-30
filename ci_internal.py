95d2c3e84dcdca5bc64ab011cf89bb18fd352be3
Leonid Yanovsky <leonid.yanovsky@intel.com>
2021-11-30 10:33:52 +0200

    Migrate CI to github actions

--- ci_internal.py
+++ ci_internal.py
@@ -1,6 +1,6 @@
-#BEGIN_LEGAL
+# BEGIN_LEGAL
 #
-#Copyright (c) 2021 Intel Corporation
+# Copyright (c) 2021 Intel Corporation
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -14,43 +14,43 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-#END_LEGAL
+# END_LEGAL
 '''run CI checks on gitlab'''
 import os
-import sys
 import platform
 import subprocess
+import sys
 from datetime import datetime
+from typing import Dict
 
 sys.path = ['scripts'] + sys.path
 
-class jobs_status_t:
-    '''record job status (success, failure and (retval,job) list'''
+
+class JobStatus:
+    """record job status (success, failure and (retval,job) list"""
+
     def __init__(self):
         self.jobs = 0
         self.fails = 0
         self.successes = 0
         self.start_time = datetime.now()
-        self.commands = [] # list of  tuples of (retval, command)
+        self.commands = []  # list of  tuples of (retval, command)
 
     def __str__(self):
-        s = []
-        s.append("JOBS: {}, SUCCESSES: {}, FAILS: {}".format(
-            self.jobs, self.successes, self.fails))
-        i = 0
-        for r, c in self.commands:
-            s.append("{}: status: {} cmd: {}".format(i, r, c))
-            i = i+1
-        return "\n".join(s) + '\n'
-    
+        s = [f"JOBS: {self.jobs}, SUCCESSES: {self.successes}, FAILS: {self.fails}"]
+
+        for index, (r, c) in enumerate(self.commands):
+            s.append(f"{index}: status: {r} cmd: {c}")
+        return os.linesep.join(s) + os.linesep
+
     def addjob(self, retval, cmd):
         self.jobs += 1
         self.commands.append((retval, cmd))
-        
+
     def fail(self, retval, cmd):
         self.fails += 1
         self.addjob(retval, cmd)
-        
+
     def success(self, retval, cmd):
         self.successes += 1
         self.addjob(retval, cmd)
@@ -63,28 +63,30 @@ class jobs_status_t:
         self.commands.extend(status.commands)
 
     def pass_rate_fraction(self):
-        return '{}/{}'.format(self.successes, self.jobs)
+        return f'{self.successes}/{self.jobs}'
 
 
 def success(status):
     '''send success SMS'''
     sys.stdout.write(str(status))
-    sys.stdout.write('[ELAPSED TIME] {}\n'.format(datetime.now() - status.start_time))
+    sys.stdout.write(f'[ELAPSED TIME] {datetime.now() - status.start_time}\n')
     sys.stdout.write("[FINAL STATUS] PASS\n")
     sys.stdout.flush()
-    #send_sms.send("XED CI: Passed ({} passing)".format(
+    # send_sms.send("XED CI: Passed ({} passing)".format(
     #    status.pass_rate_fraction()))
     sys.exit(0)
+
 
 def fail(status):
     '''send failing SMS'''
     sys.stdout.write(str(status))
-    sys.stdout.write('[ELAPSED TIME] {}\n'.format(datetime.now() - status.start_time))
+    sys.stdout.write(f'[ELAPSED TIME] {datetime.now() - status.start_time}\n')
     sys.stdout.write("[FINAL STATUS] FAIL\n")
     sys.stdout.flush()
-    #send_sms.send("XED CI: Failed ({} passing)".format(
+    # send_sms.send("XED CI: Failed ({} passing)".format(
     #    status.pass_rate_fraction()))
     sys.exit(1)
+
 
 def ensure_string(x):
     '''handle non unicode output'''
@@ -106,8 +108,8 @@ def run_subprocess(cmd, **kwargs):
     return sub.returncode, lines
 
 
-def run(status, cmd, required=False, cwd=None):
-    '''run subprocess, and record fails'''
+def run(status:JobStatus, cmd, required=False, cwd=None):
+    """run subprocess, and record fails"""
     print(cmd, flush=True)
     retval, output = run_subprocess(cmd, cwd=cwd)
     for line in output:
@@ -116,17 +118,18 @@ def run(status, cmd, required=False, cwd=None):
         print("[SUCCESS]")
         status.success(retval, cmd)
     else:
-        print("[FAIL] retval = {}".format(retval))
+        print(f"[FAIL] retval = {retval}")
         status.fail(retval, cmd)
         if required:
-            fail("Required task failed")
-    return retval == 0 # 1=ok!
+            fail(status)
+    return retval == 0  # 1=ok!
+
 
 def get_python_cmds():
     '''find python verions. return tuples of (name, command)'''
     if platform.system() == 'Windows':
         for x in ['37']:
-            p_path = 'C:/python{}/python'.format(x)
+            p_path = f'C:/python{x}/python'
             if os.path.exists(p_path):
                 return [(x, p_path)]
     if platform.system() in ['Darwin', 'Linux']:
@@ -135,160 +138,149 @@ def get_python_cmds():
     return [('dfltpython', 'python')]
 
 
-
 def all_instr(pyver, pycmd, status, kind):
     size = 'x86-64'
     linkkind = 'static'
-    build_dir = 'obj-{}-{}-{}-{}'.format(kind, pyver, size, linkkind)
+    build_dir = f'obj-{kind}-{pyver}-{size}-{linkkind}'
     cwd = os.path.abspath(os.curdir)
-    flags = '--kind {} --enc2-test-checked'.format(kind)
-    cmd = '{} xedext/xed_build.py --xed-dir {} {}  --build-dir={} host_cpu={}'.format(
-        pycmd, cwd, flags, build_dir, size)
+    flags = f'--kind {kind} --enc2-test-checked'
+    cmd = f'{pycmd} xedext/xed_build.py --xed-dir {cwd} {flags}  --build-dir={build_dir} host_cpu={size}'
     ok = run(status, cmd)
 
     if ok:
-        cmd = '{}/enc2-m64-a64/enc2tester-enc2-m64-a64 --reps 1 --main --gnuasm > a.c'.format(
-            build_dir)
+        cmd = f'{build_dir}/enc2-m64-a64/enc2tester-enc2-m64-a64 --reps 1 --main --gnuasm > a.c'
         ok = run(status, cmd)
 
-        if 1: # FIXME: ignore error code for now.
+        if 1:  # FIXME: ignore error code for now.
             cmd = 'gcc a.c'
             run(status, cmd)
 
             if ok:
-                cmd = '{}/wkit/bin/xed -i a.out > all.dis'.format(build_dir)
+                cmd = f'{build_dir}/wkit/bin/xed -i a.out > all.dis'
                 run(status, cmd)
+
 
 def archval(pyver, pycmd, status):
     size = 'x86-64'
     linkkind = 'static'
-    build_dir = 'obj-archval-{}-{}-{}'.format(pyver, size, linkkind)
+    build_dir = f'obj-archval-{pyver}-{size}-{linkkind}'
     cwd = os.path.abspath(os.curdir)
-    flags = '--kind architectural-val ' + os.getenv('ARCHVAL_OPTIONS')
-    cmd = '{} xedext/xed_build.py --xed-dir {} {} --build-dir={} host_cpu={}'.format(
-        pycmd, cwd, flags, build_dir, size)
+    flags = f"--kind architectural-val {os.getenv('ARCHVAL_OPTIONS')}"
+    cmd = f'{pycmd} xedext/xed_build.py --xed-dir {cwd} {flags} --build-dir={build_dir} host_cpu={size}'
     run(status, cmd)
 
-def get_branches_from_file():
-    f = open("misc/ci-branches.txt","r")
-    lines =  f.readlines()
+
+def get_branches_from_file() -> Dict[str, str]:
+    f = open("misc/ci-branches.txt", "r")
+    lines = f.readlines()
     f.close()
     d = {}
-    for  x in lines:
+    for x in lines:
         x = x.strip()
         a = x.split()
-        repo=a[0]
-        branch=a[1]
-        print("READING REPO: {}  TO BRANCH: {}".format(repo, branch))
-        d[repo]=branch
+        repo = a[0]
+        branch = a[1]
+        print(f"READING REPO: {repo}  TO BRANCH: {branch}")
+        d[repo] = branch
     return d
 
+
 def checkout_branches(status, branches):
-    for repo,branch in branches.items():
-        print("CHANGING REPO: {}  TO BRANCH: {}".format(repo, branch))
-        run(status, "git checkout {}".format(branch), cwd=repo)
-            
+    for repo, branch in branches.items():
+        print(f"CHANGING REPO: {repo}  TO BRANCH: {branch}")
+        run(status, f"git checkout {branch}", cwd=repo)
+
+
 def main():
-    status = jobs_status_t()
+    status = JobStatus()
     # FIXME: add knob for local use
     # git_base = 'ssh://git@gitlab.devtools.intel.com/xed-group/'
-    git_base = 'https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.devtools.intel.com/xed-group/'
 
-    mbuild_git = git_base + 'mbuild.git'
-    cmd = 'git clone {} mbuild'.format(mbuild_git)
-    run(status, cmd, required=True)
 
     # IPLDT scan XED and MBUILD
-    if 0: # disabled until get  right branch
+    if 0:  # disabled until get  right branch
         # obtain IPLDT scanner tool
         bintools_git = git_base + 'binary-tools.git'
-        cmd = 'git clone --depth 1 {} binary-tools'.format(bintools_git)
+        cmd = f'git clone --depth 1 {bintools_git} binary-tools'
         run(status, cmd, required=True)
-        
+
         # clone another copy of xed sources just for IPLDT scanning
         # FIXME: need to get the branch we are testing!
         xed_git = git_base + 'xed.git'
-        cmd = 'git clone --depth 1 {} xed'.format(xed_git)
+        cmd = f'git clone --depth 1 {xed_git} xed'
         run(status, cmd, required=True)
-        
+
         cmd = 'binary-tools/lin/ipldt3 -i xed -r ipldt-results-xed'
         run(status, cmd, required=False)
         cmd = 'cat ipldt-results-xed/ipldt_results.txt'
         run(status, cmd, required=True)
-        
+
         cmd = 'binary-tools/lin/ipldt3 -i mbuild -r ipldt-results-mbuild'
         run(status, cmd, required=False)
         cmd = 'cat ipldt-results-mbuild/ipldt_resuplts.txt'
         run(status, cmd, required=True)
-    
-    xedext_git = git_base + 'xedext.git'
-    cmd = 'git clone {} xedext'.format(xedext_git)
-    run(status, cmd, required=True)
-    
-    # check a few lines to make sure we have latest commits
-    cmd = 'git -C xedext log --oneline -5'
-    run(status, cmd, required=True)
+    if 'CI' not in os.environ:
+        git_base = 'https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.devtools.intel.com/xed-group/'
 
-    branches = get_branches_from_file()
-    checkout_branches(status, branches)
-    
-    
+        mbuild_git = git_base + 'mbuild.git'
+        cmd = f'git clone {mbuild_git} mbuild'
+        run(status, cmd, required=True)
+
+        branches = get_branches_from_file()
+        checkout_branches(status, branches)
+
     archval_repo = os.getenv('ARCHVAL_REPO')
     if archval_repo:
         archval_git = git_base + archval_repo
         short_name = os.path.splitext(archval_repo)[0]
-        cmd = 'git clone {} {}'.format(archval_git, short_name)
+        cmd = f'git clone {archval_git} {short_name}'
         run(status, cmd, required=True)
 
     for pyver, pycmd in get_python_cmds():
 
-        cmd = '{} -m pip install --user ./mbuild'.format(pycmd)
+        cmd = f'{pycmd} -m pip install --user ./mbuild'
         run(status, cmd, required=True)
 
         # {32b,64b} x {shared,dynamic} link
         for size in ['ia32', 'x86-64']:
             for linkkind, link in [('static', ''), ('dynamic', '--shared')]:
-                build_dir = 'obj-general-{}-{}-{}'.format(pyver, size, linkkind)
-                cmd = '{} mfile.py --build-dir={} host_cpu={} {} test'.format(pycmd,
-                                                                              build_dir,
-                                                                              size,
-                                                                              link)
+                build_dir = f'obj-general-{pyver}-{size}-{linkkind}'
+                cmd = f'{pycmd} mfile.py --build-dir={build_dir} host_cpu={size} {link} test'
                 run(status, cmd)
 
         # do a build with asserts enabled
-        build_dir = 'obj-assert-{}-{}'.format(pyver, 'x86-64')
-        cmd = '{} mfile.py --asserts --build-dir={} host_cpu={} test'.format(pycmd,
-                                                                             build_dir,
-                                                                             'x86-64')
+        build_dir = f'obj-assert-{pyver}-{"x86-64"}'
+        cmd = f'{pycmd} mfile.py --asserts --build-dir={build_dir} host_cpu={"x86-64"} test'
         run(status, cmd)
 
         # all instr tests
         all_instr(pyver, pycmd, status, 'internal-conf')
         all_instr(pyver, pycmd, status, 'external')
-        
+
         # arch val test
         archval(pyver, pycmd, status)
-
 
         # knc test
         if 0:
             size = 'x86-64'
             linkkind = 'static'
-            build_dir = 'obj-knc-{}-{}-{}'.format(pyver, size, linkkind)
-            cmd = '{} mfile.py --knc --build-dir={} host_cpu={} test'.format(
-                pycmd, build_dir, size)
+            build_dir = f'obj-knc-{pyver}-{size}-{linkkind}'
+            cmd = f'{pycmd} mfile.py --knc --build-dir={build_dir} host_cpu={size} test'
             run(status, cmd)
 
         if status.fails == 0:
             success(status)
         fail(status)
 
+
 def test():
-    status = jobs_status_t()
+    status = JobStatus()
     status.success(1, 'foo')
     status.success(1, 'bar')
     fail(status)
+
+
 if __name__ == "__main__":
-    #test()
+    # test()
     main()
     sys.exit(0)
